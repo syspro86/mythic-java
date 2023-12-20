7b485a53f89d3faf8196f03ed856181532da9cb5
syspro86 <syspro86@gmail.com>
2023-12-20 17:25:53 +0000

    mythic dungeon id type

--- src/main/java/net/zsoo/mythic/mythicweb/dto/MythicDungeonRepository.java
+++ src/main/java/net/zsoo/mythic/mythicweb/dto/MythicDungeonRepository.java
@@ -2,6 +2,6 @@ package net.zsoo.mythic.mythicweb.dto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MythicDungeonRepository extends JpaRepository<MythicDungeon, String> {
+public interface MythicDungeonRepository extends JpaRepository<MythicDungeon, Integer> {
 
 }
