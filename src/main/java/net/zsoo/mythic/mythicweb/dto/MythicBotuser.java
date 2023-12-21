d6f15b418cb975f552d02467a371908f87060f94
syspro86 <syspro86@gmail.com>
2023-12-21 18:41:00 +0000

    import 정리

--- src/main/java/net/zsoo/mythic/mythicweb/dto/MythicBotuser.java
+++ src/main/java/net/zsoo/mythic/mythicweb/dto/MythicBotuser.java
@@ -5,7 +5,6 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
