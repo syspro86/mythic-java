590b66aa46c5c3dbe397dd234ec8791a3cf6d847
syspro86 <syspro86@gmail.com>
2023-12-22 08:41:06 +0000

    pk가 아니게 됨

--- src/main/java/net/zsoo/mythic/mythicweb/dto/MythicBotuserPlayer.java
+++ src/main/java/net/zsoo/mythic/mythicweb/dto/MythicBotuserPlayer.java
@@ -4,8 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import lombok.Data;
 
 @Data
@@ -19,6 +19,6 @@ public class MythicBotuserPlayer {
     private String playerName;
 
     @ManyToOne
-    @PrimaryKeyJoinColumn(name = "id")
+    @JoinColumn(name = "botuserId", referencedColumnName = "id")
     private MythicBotuser botuser;
 }
